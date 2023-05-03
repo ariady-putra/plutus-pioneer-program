dafb628b28d69cd7743bbcb63eb1c3d561ca6775
Ariady Putra <ariady.putra@gmail.com>
2023-05-03 11:53:51 +0000

    Week05

--- code/Week05/homework/Homework1.hs
+++ code/Week05/homework/Homework1.hs
@@ -1,21 +1,33 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE TemplateHaskell     #-}
 
 module Homework1 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy, POSIXTime,
-                                       PubKeyHash, ScriptContext,
-                                       mkMintingPolicyScript)
-import qualified PlutusTx
-import           PlutusTx.Prelude     (Bool (False), ($))
-import           Utilities            (wrapPolicy)
+import Plutus.V1.Ledger.Interval
+import Plutus.V2.Ledger.Api -- (BuiltinData, MintingPolicy, POSIXTime,
+                                -- PubKeyHash, ScriptContext,
+                                -- mkMintingPolicyScript)
+import PlutusTx qualified
+import PlutusTx.Prelude -- (Bool (False), ($))
+
+import Utilities -- (wrapPolicy)
 
 {-# INLINABLE mkDeadlinePolicy #-}
 -- This policy should only allow minting (or burning) of tokens if the owner of the specified PubKeyHash
 -- has signed the transaction and if the specified deadline has not passed.
 mkDeadlinePolicy :: PubKeyHash -> POSIXTime -> () -> ScriptContext -> Bool
-mkDeadlinePolicy _pkh _deadline () _ctx = False -- FIX ME!
+mkDeadlinePolicy _pkh _deadline () _ctx =
+    let signedBy       = (`elem` signatories)
+        beforeDeadline = _deadline `after` validRange
+    in  traceIfFalse "Deadline has passed" beforeDeadline &&
+        traceIfFalse "Invalid signer" (signedBy _pkh)
+    where
+        info        = scriptContextTxInfo _ctx
+        signatories = txInfoSignatories info
+        validRange  = txInfoValidRange info
 
 {-# INLINABLE mkWrappedDeadlinePolicy #-}
 mkWrappedDeadlinePolicy :: PubKeyHash -> POSIXTime -> BuiltinData -> BuiltinData -> ()
--- code/Week05/homework/Homework2.hs
+++ code/Week05/homework/Homework2.hs
@@ -1,22 +1,40 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE TemplateHaskell     #-}
 
 module Homework2 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy,
-                                       ScriptContext, TokenName, TxOutRef,
-                                       mkMintingPolicyScript)
-import qualified PlutusTx
-import           PlutusTx.Prelude     (Bool (False), ($), (.))
-import           Utilities            (wrapPolicy)
+import Plutus.V1.Ledger.Value
+import Plutus.V2.Ledger.Api -- (BuiltinData, MintingPolicy,
+                                -- ScriptContext, TokenName, TxOutRef,
+                                -- mkMintingPolicyScript)
+import PlutusTx qualified
+import PlutusTx.Prelude -- (any, traceIfFalse, Bool (True, False), ($), (.), (&&), (==))
+
+import Utilities -- (wrapPolicy)
 
 {-# INLINABLE mkEmptyNFTPolicy #-}
 -- Minting policy for an NFT, where the minting transaction must consume the given UTxO as input
 -- and where the TokenName will be the empty ByteString.
 mkEmptyNFTPolicy :: TxOutRef -> () -> ScriptContext -> Bool
-mkEmptyNFTPolicy _oref () _ctx = False -- FIX ME!
+mkEmptyNFTPolicy _oref () _ctx =
+    let consumeORef    = any ((_oref==) . txInInfoOutRef) inputs
+        emptyTokenName = case flattenValue mint of
+                            [(_, tn, ta)] -> {- can't pattern match due to Exception:
+                                                Error: Unsupported feature:
+                                                Use of == from the Haskell Eq typeclass
+                                             -}
+                                "" == unTokenName tn && -- is empty ByteString TokenName?
+                                ta == 1 -- validate TokenAmount to pass `cabal test`
+                            _default -> False
+    in  traceIfFalse "UTxO not consumed" consumeORef &&
+        traceIfFalse "Invalid TokenName" emptyTokenName
+    where
+        info   = scriptContextTxInfo _ctx
+        inputs = txInfoInputs info
+        mint   = txInfoMint info
 
 {-# INLINABLE mkWrappedEmptyNFTPolicy #-}
 mkWrappedEmptyNFTPolicy :: TxOutRef -> BuiltinData -> BuiltinData -> ()
